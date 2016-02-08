c6652e78344573273f464f716a793c0ef2d0be9b
Tanjun <2225965701@qq.com>
2016-02-08 14:54:49 +0800

    Add a java project!

---
+++ HelloJava/src/com/jun/tan/HelloWorldJava.java
@@ -0,0 +1,14 @@
+package com.jun.tan;
+
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String mString = "Tan jun is good man!";
+		System.out.println(mString);
+		
+		
+	}
+
+}
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ My first project.
 Tan jun is good man.
 
 Just for testing£¡
+
+
+Add a java project!
